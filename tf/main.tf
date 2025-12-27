5723a65f8a26b782ecf2ecc83b4bc5b55cb99a67
Subhamay Bhattacharyya <142895397+bsubhamay@users.noreply.github.com>
2025-12-27 18:44:21 +0000

    feat: implement initial Terraform workflows and configurations for GCP deployment

--- tf/main.tf
+++ tf/main.tf
@@ -16,6 +16,7 @@ locals {
   }
 }
 
+# 1) Enable required APIs
 resource "google_project_service" "this" {
   for_each           = var.required_apis
   project            = var.gcp_project_id
@@ -23,21 +24,21 @@ resource "google_project_service" "this" {
   disable_on_destroy = false
 }
 
-# # 2) Create service accounts (repo/project-specific)
-# resource "google_service_account" "this" {
-#   for_each     = var.service_accounts
-#   project      = var.gcp_project_id
-#   account_id   = each.value.account_id
-#   display_name = each.value.display_name
+# 2) Create service accounts (repo/project-specific)
+resource "google_service_account" "this" {
+  for_each     = var.service_accounts
+  project      = var.gcp_project_id
+  account_id   = each.value.account_id
+  display_name = each.value.display_name
 
-#   depends_on = [google_project_service.required]
-# }
+  depends_on = [google_project_service.this]
+}
 
-# # 3) Grant project-level roles to each service account
-# resource "google_project_iam_member" "sa_roles" {
-#   for_each = local.project_iam_bindings
+# 3) Grant project-level roles to each service account
+resource "google_project_iam_member" "this" {
+  for_each = local.project_iam_bindings
 
-#   project = var.gcp_project_id
-#   role    = each.value.role
-#   member  = "serviceAccount:${google_service_account.this[each.value.sa_key].email}"
-# }
+  project = var.gcp_project_id
+  role    = each.value.role
+  member  = "serviceAccount:${google_service_account.this[each.value.sa_key].email}"
+}
--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,13 +1,23 @@
 # --- root/main.tf ---
 
-# output "tf_executor_sa_email" {
-#   value = google_service_account.sa["tf_executor"].email
-# }
-
-# output "composer_runtime_sa_email" {
-#   value = google_service_account.sa["composer_runtime"].email
-# }
-
 output "project_iam_bindings" {
   value = local.project_iam_bindings
+}
+
+output "enabled_apis" {
+  value = [for s in google_project_service.this : s.service]
+}
+
+output "service_account_emails" {
+  value = { for sa_key, sa in google_service_account.this : sa_key => sa.email }
+}
+
+output "project_iam_memberships" {
+  value = {
+    for binding_key, binding in google_project_iam_member.this :
+    binding_key => {
+      role   = binding.role
+      member = binding.member
+    }
+  }
 }
