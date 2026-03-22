3b7312323995e8bdc2d9ef644204d5ce5ddd62f8
Subhamay Bhattacharyya <subhamay.aws+github+cloudworks@gmail.com>
2026-03-21 21:24:31 -0400

    ci: update variable descriptions in variables.tf for consistency

--- infra/gcp/tf/variables.tf
+++ infra/gcp/tf/variables.tf
@@ -4,12 +4,12 @@ variable "gcp_project_id" { type = string }
 variable "gcp_region" { type = string }
 
 variable "gcp_credentials_file_path" {
-  description = "Path to the GCP service account JSON credentials file"
+  description = "Path to the GCP service account JSON credentials file."
   type        = string
 }
 
 variable "organization_id" {
-  description = "GCP Organization ID"
+  description = "GCP Organization ID."
   type        = string
 }
 
